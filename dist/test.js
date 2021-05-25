8895d0352f90fcfe4838db9e721e12ff82635bfa
himesh-wwindia <himesh@wwindia.com>
2021-05-25 13:07:12 +0530

    Latest update in gitignore

--- dist/test.js
+++
@@ -1,6 +0,0 @@
-const GenerateCommandString = require('./index').GenerateCommandString;
-
-const generateCommandString = new GenerateCommandString('VUL100', 'action.devices.commands.OnOff', true);
-const data = generateCommandString.generateCommandBody()
-
-console.log("data --->>>>>  ", data);
